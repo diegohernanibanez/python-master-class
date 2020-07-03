0a1675856dab6939d98a5f52e2b31086e2eba37c
Diego Ibañez <diego.hernan.ibanez@gmail.com>
2020-07-02 23:41:05 -0300

    funciones de prueba

--- 04.entrada-salida/entrada-salida.py
+++ 04.entrada-salida/entrada-salida.py
@@ -2,6 +2,5 @@
 nombre = input("¿Cual es tu nombre?: ")
 apellido = input("¿Cual es tu apellido?: ")
 edad = int(input("¿Cual es tu edad?: "))
-print("cualquier cosa jajaja")
 print(f"{nombre} {apellido}, tiene {edad} años")
 
---
+++ carpeta-prueba/funciones-prueba.py
@@ -0,0 +1,4 @@
+def funcion(a, b, c):
+    return a * (b + c)
+
+print(funcion(1, 2, 3))
