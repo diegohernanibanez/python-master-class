1de508f7d465075a2f81c30702549ea322ecaed7
Diego Ibañez <diego.hernan.ibanez@gmail.com>
2020-07-04 23:49:58 -0300

    se agrega la carpeta de funciones

--- 07.ejercicios/ejercicio3.py
+++ 07.ejercicios/ejercicio3.py
@@ -3,10 +3,10 @@
 numero = 1
 
 while numero <= 60 :
-    print(f"El cuadrado de {numero} es {numero * numero}")
+    print(f"El cuadrado de {numero} es {numero ** 2}")
     numero += 1
 
 print("------------------------")
 
 for i in range(1, 61):
-    print(f"El cuadrado de {i} es {i * i}")
+    print(f"El cuadrado de {i} es {i ** 2}")
